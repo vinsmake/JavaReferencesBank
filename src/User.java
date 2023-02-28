74a85b90d4d0dffb8e486ac747bd6b641803e80d
vinsmake <ps.enrique.e@gmail.com>
2023-02-28 11:28:45 -0600

    chore | Added an extension of foreignUser.

--- src/User.java
+++ src/User.java
@@ -6,29 +6,70 @@ public class User {
     private static int userCounter = 0; //static makes the data independient of the new creation. The user counter increase when a object is created, so it gives the user a unic number.
 
     //ask for all the data
-    public User(String username, String password, String name, int id, String location, long cardNumber, int cardCvv, int cardExpiration) {
+    public User(String username, String password, String name, int id, String location, String cardNumber, int cardCvv, int cardExpiration) {
         //User
-        this.username = username;
+
+        //user name
+        if (username.length() >= 6){
+            this.username = username;
+        } else {
+            System.out.println("Invalid username, must have 8+ letters and none numbers");
+        };
+        
         System.out.println("The User name is: " + this.username);
+
+        //password
+        if (password.length() >= 6) {
         this.password = password;
+        } else {
+            System.out.println("Invalid password, must have 6+ letters");
+        };
         System.out.println("The Password is: " + this.password);
+
+
+        //number of user
         userCounter++;
         this.number = userCounter;
         System.out.println("The number of user is: " + this.number);
+
+
             //Client
+            //client name
             this.client.setName(name);
             System.out.println("The Name is: " + this.client.getName());
+
+
+            //client id
             this.client.setId(id);
             System.out.println("The Id is: " + this.client.getId());
+
+
+            //client location
             this.client.setLocation(location);
             System.out.println("The Location is: " + this.client.getLocation());
-                //Account
+
+
+                //Account 
+                //Account  card number
+                if (cardNumber.length() == 16) {
                 this.client.getAccount().setNumber(cardNumber);
+                } else {
+                    System.out.println("Error: the card numbers must have 16 numbers, but you wrote " + cardNumber.length());
+                }
                 System.out.println("The Card Number is: " + this.client.getAccount().getNumber());
+
+
+                //Account card cvv
                 this.client.getAccount().setCvv(cardCvv);
                 System.out.println("The Card Cvv is: " + this.client.getAccount().getCvv());
+
+
+                //Account card expiration date
                 this.client.getAccount().setExpiration(cardExpiration);
                 System.out.println("The Card Expiration is: " + this.client.getAccount().getExpiration());
+
+
+        //total accounts registered 
         System.out.println("There are " + userCounter + " user registered");
     }
     
